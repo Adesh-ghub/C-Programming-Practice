6e11cbe6359b0623ee0b261acb5f9b24c9fc84a0
Adesh-ghub <work.adesh14@gmail.com>
2026-07-31 16:15:24 +0530

    Add input output program

---
+++ 01_Basics/02_input_output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter first no:");
+    scanf("%d",&a);
+    printf("Enter second no:");
+    scanf("%d",&b);
+    printf("First no is:%d \n",a);
+    printf("Second no is:%d",b);
+    return 0;
+}
