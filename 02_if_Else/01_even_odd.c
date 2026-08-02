6e8ac9008d9f538fb18e85039a0b5f2149243442
Adesh-ghub <work.adesh14@gmail.com>
2026-08-02 19:12:11 +0530

    Add even odd program

---
+++ 02_if_Else/01_even_odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter no");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("even no");
+    }
+    else
+    printf("odd no");
+    return 0;
+}
