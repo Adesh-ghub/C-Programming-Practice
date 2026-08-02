0c3660211bf6e35a74e94a6d74f3c5b2900e6fc2
Adesh-ghub <work.adesh14@gmail.com>
2026-08-02 11:46:44 +0530

    Add arithmetic operations program

---
+++ 01_Basics/03_arithmetic_operations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter first no:");
+    scanf("%d",&a);
+    printf("Enter second no:");
+    scanf("%d",&b);
+    printf("Addition=%d\n",a+b);
+    printf("Subtraction=%d\n",a-b);
+    printf("multiplication=%d\n",a*b);
+    printf("Div:%d\n",a/b);
+    printf("modulus:%d",a%b);
+
+    return 0;
+}
